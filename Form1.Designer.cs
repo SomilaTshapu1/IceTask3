73410ec31d54acf562eb632e136fc9c741c1949c
SomilaTshapu1 <82814273+SomilaTshapu1@users.noreply.github.com>
2021-06-10 15:12:56 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace ICE3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label = new System.Windows.Forms.Label();
+            this.btnCapture = new System.Windows.Forms.Button();
+            this.ClassList = new System.Windows.Forms.ListBox();
+            this.txtStudentName = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(60, 179);
+            this.label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(125, 25);
+            this.label.TabIndex = 0;
+            this.label.Text = "Student Name";
+            // 
+            // btnCapture
+            // 
+            this.btnCapture.Location = new System.Drawing.Point(77, 251);
+            this.btnCapture.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnCapture.Name = "btnCapture";
+            this.btnCapture.Size = new System.Drawing.Size(107, 38);
+            this.btnCapture.TabIndex = 2;
+            this.btnCapture.Text = "Capture";
+            this.btnCapture.UseVisualStyleBackColor = true;
+            this.btnCapture.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // ClassList
+            // 
+            this.ClassList.FormattingEnabled = true;
+            this.ClassList.ItemHeight = 25;
+            this.ClassList.Location = new System.Drawing.Point(408, 158);
+            this.ClassList.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ClassList.Name = "ClassList";
+            this.ClassList.Size = new System.Drawing.Size(228, 129);
+            this.ClassList.TabIndex = 3;
+            // 
+            // txtStudentName
+            // 
+            this.txtStudentName.Location = new System.Drawing.Point(243, 176);
+            this.txtStudentName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtStudentName.Name = "txtStudentName";
+            this.txtStudentName.Size = new System.Drawing.Size(141, 31);
+            this.txtStudentName.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(215, 251);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 38);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Clear List";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(262, 85);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(162, 48);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Class List";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(530, 104);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.Size = new System.Drawing.Size(8, 8);
+            this.dataGridView1.TabIndex = 8;
+            this.dataGridView1.Text = "dataGridView1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtStudentName);
+            this.Controls.Add(this.ClassList);
+            this.Controls.Add(this.btnCapture);
+            this.Controls.Add(this.label);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Button btnCapture;
+        private System.Windows.Forms.ListBox ClassList;
+        private System.Windows.Forms.TextBox txtStudentName;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ICE3
+{
+    public partial class Form1 : Form
+    {
+        private static string path = System.IO.Path.GetFullPath(@"..\..\..\") + "ClassList.txt";
+
+        string[] ProductList;
+
+        string name;
+        string del = ",";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void RunDelimiter()
+        {
+
+            name = txtStudentName.Text;
+
+            ProductList = name.Split(del);
+
+            foreach (var Product in ProductList)
+            {
+                ClassList.Items.Add(Product);
+            }
+            txtStudentName.Clear();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (txtStudentName.Text != "")
+            {
+                RunDelimiter();
+            }
+            else
+            {
+                MessageBox.Show("Values can't be empty");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ClassList.Items.Clear();
+        }
+    }
+}
